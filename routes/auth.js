b4aa2efa54d2fc9c1a264c9b9bf65eac32fc8119
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-06-29 12:53:29 +0100

    Configure the Google strategy in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,72 @@ const express = require("express");
 //require in passport, the Google strategy and the SQL db
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oidc");
+
+//configure the Google strategy
+
+assport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env["GOOGLE_CLIENT_ID"],
+			clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
+			callbackURL: "/oauth2/redirect/google",
+			scope: ["profile"],
+		},
+		function verify(issuer, profile, cb) {
+			db.get(
+				"SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+				[issuer, profile.id],
+				function (err, row) {
+					if (err) {
+						return cb(err);
+					}
+					if (!row) {
+						db.run(
+							"INSERT INTO users (name) VALUES (?)",
+							[profile.displayName],
+							function (err) {
+								if (err) {
+									return cb(err);
+								}
+
+								let id = this.lastID;
+								db.run(
+									"INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+									[id, issuer, profile.id],
+									function (err) {
+										if (err) {
+											return cb(err);
+										}
+										let user = {
+											id: id,
+											name: profile.displayName,
+										};
+										return cb(null, user);
+									}
+								);
+							}
+						);
+					} else {
+						db.get(
+							"SELECT * FROM users WHERE id = ?",
+							[row.user_id],
+							function (err, row) {
+								if (err) {
+									return cb(err);
+								}
+								if (!row) {
+									return cb(null, false);
+								}
+								return cb(null, row);
+							}
+						);
+					}
+				}
+			);
+		}
+	)
+);
+
 const db = require("../db");
 
 const router = express.Router();

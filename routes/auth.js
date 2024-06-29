5251f8c9a42da7695b2c39be313c1311754f5aa8
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-06-29 12:26:13 +0100

    Install passport and passport-google-oidc
    Also require into auth.js the above and additional ./db

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 //create a login route to render a sign in page
 const express = require("express");
 
+//require in passport, the Google strategy and the SQL db
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oidc");
+const db = require("../db");
+
 const router = express.Router();
 
 router.get("/login", function (req, res, next) {
